ef93d3a34fe4e063471f5ea07026425f0b559174
Lucas Sousa Alves <91626518+LucasSousaAl@users.noreply.github.com>
2026-01-14 23:07:44 +0000

    add pages

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return <h1>¹ O Senhor é o meu pastor, nada me faltará.
+        ² Deitar-me faz em verdes pastos, guia-me mansamente a águas tranquilas.
+        ³ Refrigera a minha alma; guia-me pelas veredas da justiça, por amor do seu nome.
+        ⁴ Ainda que eu andasse pelo vale da sombra da morte, não temeria mal algum, porque tu estás comigo; a tua vara e o teu cajado me consolam.
+        ⁵ Preparas uma mesa perante mim na presença dos meus inimigos, unges a minha cabeça com óleo, o meu cálice transborda.
+        ⁶ Certamente que a bondade e a misericórdia me seguirão todos os dias da minha vida; e habitarei na casa do Senhor por longos dias.
+        Salmos 23:1-6</h1>;
+}
